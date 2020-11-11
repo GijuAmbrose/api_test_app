5ebedd6cf8079762cd8115a255fbd6827a5053fe
Giju Ambrose <jijoakalarikkal@gmail.com>
2020-11-11 15:47:16 +0530

    api test app first commit

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :set_article, only: [:create, :update]
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  # GET /comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+    @comment = @article.comments.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.require(:comment).permit(:comment, :article_id)
+    end
+
+    def set_article
+      @article = Article.find(params["article_id"])
+    end
+end
